894522315850c32cde1e2c127b20b2d752754e79
bilalMavus <bilalmavuss@gmail.com>
2023-02-21 20:41:47 +0300

    merhaba kodunu yazdım 2

--- src/gitdeneme.java
+++ src/gitdeneme.java
@@ -2,6 +2,7 @@ public class gitdeneme {
     public static void main(String[] args) {
         System.out.println("merhaba");
         System.out.println("merhaba1");
+        System.out.println("merhaba2");
 
 
     }
